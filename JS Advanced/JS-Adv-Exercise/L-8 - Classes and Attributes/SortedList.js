a01a07dddfb4916b9e08a54e909068bc77db6bbe
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-01-09 08:39:43 +0200

    add

--- JS Advanced/JS-Adv-Exercise/L-8 - Classes and Attributes/SortedList.js
+++ JS Advanced/JS-Adv-Exercise/L-8 - Classes and Attributes/SortedList.js
@@ -1,5 +1,31 @@
 class List {
+    constructor() {
+        this.arrNumber = [];
+        this.size = this.arrNumber.length;
+    }
 
+    add(element) {
+        this.arrNumber.push(element);
+        this.arrNumber.sort((a, b) => a - b);
+        this.size = this.arrNumber.length;
+    }
+
+    remove(index) {
+        if(index >= 0 && index < this.arrNumber.length) {
+            this.arrNumber.splice(index, 1);
+            this.size = this.arrNumber.length;
+        } else {
+            throw new Error("Index doesn't exist");
+        }
+    }
+
+    get(index) {
+        if(index >= 0 && index < this.arrNumber.length) {
+            return this.arrNumber[index];
+        } else {
+            throw new Error("Index doesn't exist");
+        }
+    }
 }
 
 
@@ -9,4 +35,4 @@ list.add(6);
 list.add(7);
 console.log(list.get(1)); 
 list.remove(1);
-console.log(list.get(1));
+console.log(list.get(3));
