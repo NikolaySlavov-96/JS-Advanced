cf0d593475573549c3cc18a02299364fe7b152ce
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-01-16 22:05:19 +0200

    add

--- JS Advanced/JS-Adv-Lab/L-5 - DOM Manipulation and Events/06. Dynamic-Validation/app.js
+++ JS Advanced/JS-Adv-Lab/L-5 - DOM Manipulation and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,22 @@
 function validate() {
-    console.log('TODO:...');
+    const current = document.getElementById('email');
+    current.addEventListener('change', onChecked);
+
+
+    function onChecked() {
+
+        const emeil = current.value;
+        const patternEmail = /[a-z0-9]+[@]+[a-z0-9]+[.]+[a-z0-9]+/g;
+
+        const result = patternEmail.test(emeil);
+        console.log(result)
+
+        if(result) {
+            // current.value = '';w
+            current.className = '';
+        } else {
+            current.className = 'error'
+        }
+
+    }
 }
--- JS Advanced/JS-Adv-Lab/L-5 - DOM Manipulation and Events/07. Shopping-Cart/solution.js
+++ JS Advanced/JS-Adv-Lab/L-5 - DOM Manipulation and Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,46 @@
 function solve() {
    //TODO...
+   const shopingCart = document.querySelector('.shopping-cart');
+   shopingCart.addEventListener('click', onLoad);
+   const textOutput = document.querySelector('textarea');
+   const checkOut = document.querySelector('.checkout');
+   checkOut.addEventListener('click', checkPay);
+
+
+   const store = {};
+   const displayProduct = [];
+
+   function onLoad(ev){
+
+      if(ev.target.className === 'add-product'){
+         const line = ev.target.parentElement.parentElement;
+         const productName = line.querySelector('.product-title').textContent;
+         const productPrice = line.querySelector('.product-line-price').textContent;
+         
+         displayProduct.push(`Added ${productName} for ${productPrice} to the cart.`)
+
+         if(!store[productName]) {
+            store[productName] = 0;
+         }
+         store[productName] += Number(productPrice);
+
+         textOutput.textContent = displayProduct.join('\n')
+      }
+   }
+
+   function checkPay() {
+      const button = Array.from(document.querySelectorAll('button')).map(b => b.disabled = true);
+
+      let totalPrice = 0;
+      const products = [];
+
+      for (const product in store) {
+         totalPrice += Number(store[product]);
+         products.push(product);
+      }
+
+      displayProduct.push(`You bought ${products.join(`, `)} for ${totalPrice.toFixed(2)}.`)
+
+      textOutput.textContent = displayProduct.join('\n')
+   }
 }
