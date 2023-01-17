7da3aae6204d010ddde95943b74be5fafd5c031b
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-01-17 22:02:32 +0200

    add

--- JS Advanced/JS-Adv-Exercise/L-4 - DOM Introduction/09. Number Convertor/solution.js
+++ JS Advanced/JS-Adv-Exercise/L-4 - DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,40 @@
 function solve() {
+    
+    const selectMenuTo = document.getElementById('selectMenuTo');
+    const MenuTo = document.querySelectorAll('select option')[1];
+    MenuTo.value = 'binary';
+    MenuTo.textContent = 'Binary';
 
-    //TODO...
+    let option = document.createElement('option');
+    option.value = 'hexadecimal';
+    option.textContent = 'Hexadecimal';
+
+    selectMenuTo.appendChild(option);
+
+    const inputNumber = document.getElementById('input');
+    const outputNumber = document.getElementById('result');
+    const button = document.querySelector('button');
+    button.addEventListener('click',convert);
+
+    function convert(ev) {
+
+        let numberInput = Number(inputNumber.value) //
+        const current = selectMenuTo.value;
+
+        if(current === 'hexadecimal') {
+            hexadecimal(numberInput);
+        } else if(current === 'binary') {
+            binary(numberInput);
+        }
+    }
+
+    function hexadecimal(numberInput) {
+
+        outputNumber.value = numberInput.toString(16).toUpperCase()
+    }
+
+    function binary(numberInput) {
+
+        outputNumber.value = numberInput.toString(2);
+    }
 }
