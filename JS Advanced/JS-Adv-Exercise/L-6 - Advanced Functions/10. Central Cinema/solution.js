7839afd6d17fde5c17b49fdaef5b681f8bd33329
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-01-19 23:05:24 +0200

    add

--- JS Advanced/JS-Adv-Exercise/L-6 - Advanced Functions/10. Central Cinema/solution.js
+++ JS Advanced/JS-Adv-Exercise/L-6 - Advanced Functions/10. Central Cinema/solution.js
@@ -1,3 +1,65 @@
 function solve() {
-    // TODO
+
+    const button = document.querySelector('#container button');
+    button.addEventListener('click', onCheck);
+    const inputData = document.querySelectorAll('#container input');
+    const moviesUl = document.querySelector('#movies ul');
+
+    const cinema = {};
+
+
+    function onCheck(ev) {
+        ev.preventDefault();
+
+        // const [name, hall, price] = inputData;
+        const name = inputData[0].value;
+        const hall = inputData[1].value;
+        const price = inputData[2].value;
+        
+        if(name !== '' && hall !== '' && price !== '' && !isNaN(price)) {
+
+            //problem is secand decimal 
+
+            const li = create('li');
+            li.appendChild(create('span', name));
+            li.appendChild(create('strong', `Hall: ${hall}`));
+
+            const div = create('div');
+            div.appendChild(create('strong', price));
+            div.appendChild(create('input', '', 'Tickets Sold'));
+            const btnL = create('button', 'Archive');
+            div.appendChild(btnL);
+            li.appendChild(div);
+            moviesUl.appendChild(li);
+
+            btnL.addEventListener('click', onCheckBillet);
+            cinema[name] = price;
+            
+            inputData.forEach(e => e.value = '');
+        }
+    }
+
+    function create(type, text, oth) { 
+        const elm = document.createElement(type);
+
+        if(text !== undefined || text !== '') {
+            elm.textContent = text;            
+        }
+        
+        if(oth !== undefined) {
+            elm.placeholder = oth;
+        }
+
+        return elm;
+    }
+
+    function onCheckBillet() {
+        const inputCounter = document.querySelector('#movies div input');
+        const span = document.querySelector('#movies span');
+
+        if(!isNaN(inputCounter.value)) {
+            let allPrice = cinema[span.textContent] * inputCounter.value;
+            console.log(allPrice)
+        }
+    }
 }
