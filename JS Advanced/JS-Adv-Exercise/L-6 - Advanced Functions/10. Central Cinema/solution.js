4177f18d291bb145abc9db103d498d3983a70972
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-01-20 23:14:53 +0200

    add

--- JS Advanced/JS-Adv-Exercise/L-6 - Advanced Functions/10. Central Cinema/solution.js
+++ JS Advanced/JS-Adv-Exercise/L-6 - Advanced Functions/10. Central Cinema/solution.js
@@ -3,23 +3,28 @@ function solve() {
     const button = document.querySelector('#container button');
     button.addEventListener('click', onCheck);
     const inputData = document.querySelectorAll('#container input');
+
     const moviesUl = document.querySelector('#movies ul');
+    moviesUl.addEventListener('click', onArchive);
+    const archiveUl = document.querySelector('#archive ul');
+    archiveUl.addEventListener('click', onDelete);
+
+    const allClear = document.querySelector('#archive button');
+    allClear.addEventListener('click', clearAll);
 
     const cinema = {};
-
-
+    
     function onCheck(ev) {
         ev.preventDefault();
-
-        // const [name, hall, price] = inputData;
+        
         const name = inputData[0].value;
         const hall = inputData[1].value;
-        const price = inputData[2].value;
+        let price = inputData[2].value;
         
         if(name !== '' && hall !== '' && price !== '' && !isNaN(price)) {
-
-            //problem is secand decimal 
-
+            
+            price = Number(price).toFixed(2);
+        
             const li = create('li');
             li.appendChild(create('span', name));
             li.appendChild(create('strong', `Hall: ${hall}`));
@@ -27,39 +32,58 @@ function solve() {
             const div = create('div');
             div.appendChild(create('strong', price));
             div.appendChild(create('input', '', 'Tickets Sold'));
-            const btnL = create('button', 'Archive');
-            div.appendChild(btnL);
+            div.appendChild(create('button', 'Archive'));
             li.appendChild(div);
             moviesUl.appendChild(li);
 
-            btnL.addEventListener('click', onCheckBillet);
             cinema[name] = price;
             
             inputData.forEach(e => e.value = '');
         }
     }
+    
+    function onArchive(ev) {
+
+        const li = ev.target.parentElement.parentElement;
+        const inputCounter = li.querySelector('input');
+        const span = li.querySelector('span');
+
+        const strong = li.querySelector('strong');
+        const div = li.querySelector('div');
+        
+        if(!isNaN(Number(inputCounter.value)) && ev.target.tagName === 'BUTTON' && inputCounter.value !== '') {
+            let allPrice = cinema[span.textContent] * inputCounter.value;
+            
+            strong.textContent = `Total amount: ${(allPrice).toFixed(2)}`
+            li.removeChild(div);
+            li.appendChild(create('button', 'Delete'));
+            
+            archiveUl.appendChild(li);
+        }
+    }
+    
+    function onDelete(ev) {
+        const li = ev.target.parentElement;
+        if(ev.target.tagName === 'BUTTON') {
+            li.remove()
+        }
+    }
+
+    function clearAll(ev) {
+        archiveUl.innerHTML = '';
+    }
 
     function create(type, text, oth) { 
         const elm = document.createElement(type);
-
+        
         if(text !== undefined || text !== '') {
             elm.textContent = text;            
         }
         
-        if(oth !== undefined) {
+        if(oth !== undefined || oth !== '') {
             elm.placeholder = oth;
         }
-
+    
         return elm;
-    }
-
-    function onCheckBillet() {
-        const inputCounter = document.querySelector('#movies div input');
-        const span = document.querySelector('#movies span');
-
-        if(!isNaN(inputCounter.value)) {
-            let allPrice = cinema[span.textContent] * inputCounter.value;
-            console.log(allPrice)
-        }
     }
 }
