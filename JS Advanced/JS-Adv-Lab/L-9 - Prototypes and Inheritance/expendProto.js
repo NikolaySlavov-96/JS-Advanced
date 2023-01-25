5764e1c28e4f69fb7286306e22d190bdec0335f8
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-01-25 21:47:15 +0200

    add new task

---
+++ JS Advanced/JS-Adv-Lab/L-9 - Prototypes and Inheritance/expendProto.js
@@ -0,0 +1,23 @@
+function expendPrototype(classDef) {
+    classDef.prototype.species = 'Human';
+    classDef.prototype.toSpeciesString = function() {
+        return `I am a ${this.species}. ${this.toString()}`
+    }
+}
+
+
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.toString = function() {
+    return `To string method from prototype of ${this.name}`
+}
+
+expendPrototype(Person);
+
+const p = new Person('Peter', 34);
+console.log(p.toSpeciesString());
+console.log(p.species);
