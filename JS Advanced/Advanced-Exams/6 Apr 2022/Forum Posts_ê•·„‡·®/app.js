25799e102e98100b4b108c3a6192aece8c93bb81
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-01-29 20:51:05 +0200

    reEdit code and change methods

--- JS Advanced/Advanced-Exams/6 Apr 2022/Forum Posts_ê•·„‡·®/app.js
+++ JS Advanced/Advanced-Exams/6 Apr 2022/Forum Posts_ê•·„‡·®/app.js
@@ -11,52 +11,80 @@ function solve() {
     const btnClear = document.getElementById('clear-btn');
 
     btnPublich.addEventListener('click', onCheck);
-    review.addEventListener('click', onRewie);
+    // review.addEventListener('click', onRewie);
     btnClear.addEventListener('click', onClear);
     const store = []
 
     function onCheck(ev) {
         ev.preventDefault();
 
-        if(postTitle.value === '' || postCategory.value === '' || postContent.value === '') {
+        const title = postTitle.value;
+        const category = postCategory.value;
+        const content = postContent.value;
+
+        if(title === '' || category === '' || content === '') {
             return;
         }
         const li = create('li', 'class', 'rpost');
-        const article = create('article');
-        article.appendChild(create('h4', '', '', `${postTitle.value}`))
-        article.appendChild(create('p', '', '', `Category: ${postCategory.value}`));
-        article.appendChild(create('p', '', '', `Content: ${postContent.value}`));
-        li.appendChild(article);
-        li.appendChild(create('button', 'class', 'action-btn edit', 'Edit'));
-        li.appendChild(create('button', 'class', 'action-btn aprrove', 'Aprrove'));
+        li.innerHTML = `<article>
+            <h4>${title}</h4>
+            <p>Category: ${category}</p>
+            <p>Content: ${content}</p>
+        </article>
+        <button class="action-btn edit">Edit</button>
+        <button class="action-btn approve">Approve</button>`;
+        // const article = create('article');
+        // article.appendChild(create('h4', '', '', `${postTitle.value}`))
+        // article.appendChild(create('p', '', '', `Category: ${postCategory.value}`));
+        // article.appendChild(create('p', '', '', `Content: ${postContent.value}`));
+        // li.appendChild(article);
+        // const edit = create('button', 'class', 'action-btn', 'Edit');
+        // edit.classList.add('edit');
+        // li.appendChild(edit);
+        // li.appendChild(create('button', 'class', 'action-btn aprrove', 'Aprrove'));
+
         review.appendChild(li);
-
-        store.push({name: postTitle.value, category: postCategory.value, content: postContent.value});
-
+        
         postTitle.value = '';
         postCategory.value = '';
         postContent.value = '';
+        
+        const btnEdit = li.querySelector('.edit');
+        const btnApprove = li.querySelector('.approve');
+        
+        btnEdit.addEventListener('click', (ev) => {
+            postTitle.value = title;
+            postCategory.value = category;
+            postContent.value = content;
+            li.remove();
+        });
+        btnApprove.addEventListener('click', () => {
+            btnEdit.remove();
+            btnApprove.remove();
+            publish.appendChild(li);
+        });
     }
 
-    function onRewie(ev) {
-        if(ev.target.tagName === 'BUTTON') {
-            const li = ev.target.parentElement;
-            if(ev.target.textContent === 'Edit') {
-                const article = ev.target.parentElement.children[0].children[0].textContent;
-                const info = store.find(el => el.name === article);
-                postTitle.value = info.name;
-                postCategory.value = info.category;
-                postContent.value = info.content
-                li.remove();
-            } else if(ev.target.textContent === 'Aprrove') {
-                li.removeChild(li.children[1]);
-                li.removeChild(li.children[1]);
-                publish.appendChild(li);
-            }
-        }
-    }
+    // function onRewie(ev) {
+    //     if(ev.target.tagName === 'BUTTON') {
+    //         const li = ev.target.parentElement;
+    //         if(ev.target.textContent === 'Edit') {
+    //             const article = ev.target.parentElement.children[0].children[0].textContent;
+    //             const info = store.find(el => el.name === article);
+    //             postTitle.value = info.name;
+    //             postCategory.value = info.category;
+    //             postContent.value = info.content
+    //             li.remove();
+    //         } else if(ev.target.textContent === 'Aprrove') {
+    //             li.removeChild(li.children[1]);
+    //             li.removeChild(li.children[1]);
+    //             publish.appendChild(li);
+    //         }
+    //     }
+    // }
 
-    function onClear() {
+    function onClear(ev) {
+        ev.preventDefault();
         publish.innerHTML = '';
     }
 
