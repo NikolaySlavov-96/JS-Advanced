2296c3d7b50a28ebf55d27d607079c39de6e701d
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-02-01 08:48:45 +0200

    write first class

---
+++ JS Advanced/JS-Adv-Exercise/L-9 - Prototypes and Inheritance/Posts.js
@@ -0,0 +1,40 @@
+function solution() {
+    class Post {
+        constructor(title, content) {
+            this.title = title;
+            this.content = content;
+        }
+        toString() {
+            return `Post: ${this.title} \nContent: ${this.content}`
+        }
+    }
+
+    class SocialMediaPost extends Post {
+        constructor() {
+            
+        }
+    }
+
+    return {
+        Post
+    }
+}
+
+const classes = solution();
+let post = new classes.Post("Post", "Content");
+console.log(post.toString());
+// Post: Post
+// Content: Contentlet 
+scm = new classes.SocialMediaPost("TestTitle", "TestContent", 25, 30);
+scm.addComment("Good post");
+scm.addComment("Very good post");
+scm.addComment("Wow!");
+
+// console.log(scm.toString());
+// // Post: TestTitle
+// // Content: TestContent
+// // Rating: -5
+// // Comments:
+// //  * Good post
+// //  * Very good post
+// //  * Wow!
