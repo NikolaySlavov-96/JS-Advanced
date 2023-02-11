615b0bc3149c888a869522d4aed2ee98dcab52f4
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-02-11 08:40:21 +0200

    created get request

--- JS Applications/L-1 - Asynchronous Programming/06.Blog/app.js
+++ JS Applications/L-1 - Asynchronous Programming/06.Blog/app.js
@@ -1,5 +1,42 @@
 function attachEvents() {
-    console.log('TODO...');
+    
+    document.getElementById(`btnLoadPosts`).addEventListener('click', onGetPosts);
+    document.getElementById(`btnViewPost`).addEventListener('click', onViewPost)
+
+    const selecPost = document.getElementById(`posts`);
+
+    async function onGetPosts() {
+
+        try {
+            const respons = await fetch('http://localhost:3030/jsonstore/blog/posts');
+    
+            if(respons.status !== 200) {
+                throw new Error('@')
+            }
+
+            const data = await respons.json();
+            const dataArr = Object.values(data)
+
+            for (const dataLine of dataArr) {
+                selecPost.appendChild(createOption(dataLine.id, dataLine.title));
+            }
+
+        } catch(err) {
+            console.log(err.message)
+        }
+
+    }
+
+    async function onViewPost() {
+
+    }
+
+    function createOption(inputValut, contentTitle) {
+        const el = document.createElement('option');
+        el.value = inputValut
+        el.textContent = contentTitle;
+        return el;
+    }
 }
 
 attachEvents();
